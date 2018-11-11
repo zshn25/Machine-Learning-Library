fc1dd8828d54210c8f1b33497618435c5b2da25c
zshn25 <5270999+zshn25@users.noreply.github.com>
2018-11-11 12:32:37 +0100

    data normalization

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -63,6 +63,12 @@ Yvalidation = Ytrain(end-49:end,:);
 
 Xtrain = Xtrain(1:end-50,:);
 Ytrain = Ytrain(1:end-50,:);
+
+% Normalize training data
+Xtrain = (Xtrain - mean(Xtrain,1)) ./ std(Xtrain,1);
+Ytrain = (Ytrain - mean(Ytrain,1)) ./ std(Ytrain,1);
+
+
 [N, D] = size(Xtrain);
 % Training with Lasso Regression with polynomial basis
 Lambda = 0.1;
@@ -70,12 +76,13 @@ basis = [0,1,2,3];
 
 % Basis functions: Polynomial: [1 x, x^2, x^3]
 % DesignMatrix = [ones(N,1), Xtrain, Xtrain.^2, Xtrain.^3];
+DesignMatrix = [];
 for b = basis
     if b == 0
         DesignMatrix = [DesignMatrix, ones(N,1)];
         continue;
     end
-    DesignMatrix = [DesignMatrix, X.^b];
+    DesignMatrix = [DesignMatrix, Xtrain.^b];
 end
 % Decided to use Lasso Regression for tion of useful features only
 w = LassoRegression(DesignMatrix, Ytrain, Lambda);
