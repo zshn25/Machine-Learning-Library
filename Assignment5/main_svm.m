932bc19f77684374a43551ae825b350850f3b5ea
Zeeshan <zsuri@mpi-inf.mpg.de>
2018-11-26 10:53:17 +0100

    corrected

--- Assignment5/main_svm.m
+++ Assignment5/main_svm.m
@@ -4,8 +4,10 @@ C = [10,100,200,500];
 
 for c = C
     [alpha, w, TrainErrs, TestErrs] = CoordinateDescentSVM(Xtrain, ytrain, c, Xtest, ytest);
-    plot(1:numel(TrainErrs),TrainErrs,'b:');
+    plot(1:numel(TrainErrs),TrainErrs,'b.-');
+    title(['Training Error Rate for C: ', num2str(c)]), xlabel('Iterations'), ylabel('Error');
     saveas(gcf, ['DIGITS01TrainErrs_' num2str(c)], 'png');
     plot(1:numel(TrainErrs),TestErrs,'g.-');
+    title(['Test Error Rate for C: ', num2str(c)]), xlabel('Iterations'), ylabel('Error');
     saveas(gcf, ['DIGITS01TestErrs_' num2str(c)], 'png');
 end
