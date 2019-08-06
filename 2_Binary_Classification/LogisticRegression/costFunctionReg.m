0440c8e4269232e173d61eafb040bf87417e0704
Zeeshan <zshn25@gmail.com>
2019-08-06 13:25:14 +0200

    added logistic regression

---
+++ 2_Binary_Classification/LogisticRegression/costFunctionReg.m
@@ -0,0 +1,24 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+[J, grad] = costFunction(theta, X, y); % Add regularization term and its gradient to the unregularized version
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+J = J + lambda*0.5*(sum(theta(2:end).^2))/m;
+grad = grad + [0; lambda * (theta(2:end) / m)];
+
+% =============================================================
+
+end
